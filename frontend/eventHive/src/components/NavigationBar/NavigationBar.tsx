e3b8360f6c581a9d8bf464f20f473d649d44cf16
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-23 15:31:42 +0300

    navigation bar

---
+++ frontend/eventHive/src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,14 @@
+
+import EventHiveLogo from "../../assets/EventHiveLogo.svg";
+
+
+function NavigationBar() {
+ 
+  return (
+    <div>
+      <img src={EventHiveLogo} alt="" />
+    </div>
+  );
+}
+
+export default NavigationBar;
---
+++ frontend/eventHive/src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+
+import NavigationBar from "../../components/NavigationBar/NavigationBar";
+
+const Home = () => {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default Home;
