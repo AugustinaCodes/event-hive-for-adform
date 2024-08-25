3262a78bf29e5e2642b1c2c1ce61ffe36054c0b2
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-25 20:42:15 +0300

    set up basic navigation bar for mobile and desktop screens

--- frontend/eventHive/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/eventHive/src/components/NavigationBar/NavigationBar.tsx
@@ -1,13 +1,65 @@
-
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  useMediaQuery,
+} from "@mui/material";
+import EventHiveTentLogo from "../../assets/EventHiveTentLogo.svg";
 import EventHiveLogo from "../../assets/EventHiveLogo.svg";
-
+import MenuIcon from "@mui/icons-material/Menu";
+import styles from "./NavigationBar.module.scss";
+import { useNavigate } from "react-router-dom";
 
 function NavigationBar() {
- 
+  const navigate = useNavigate();
+  const isDesktop = useMediaQuery(`(min-width:900px)`);
+
   return (
-    <div>
-      <img src={EventHiveLogo} alt="" />
-    </div>
+    <AppBar
+      position="static"
+      color="transparent"
+      elevation={0}
+      sx={{ border: "none" }}
+    >
+      <Toolbar
+        disableGutters
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <img
+          className={styles.mobileLogo}
+          src={EventHiveTentLogo}
+          alt="Event Hive Tent Logo"
+          onClick={() => navigate("/")}
+        />
+        <img
+          className={styles.desktopLogo}
+          src={EventHiveLogo}
+          alt="Event Hive Logo"
+          onClick={() => navigate("/")}
+        />
+        <Container
+          sx={{ display: "flex", justifyContent: "flex-end", padding: "0" }}
+        >
+          <Button color="inherit" sx={{ fontWeight: "bold" }}>
+            Login
+          </Button>
+          <Button color="inherit" sx={{ fontWeight: "bold" }}>
+            Sign Up
+          </Button>
+          {!isDesktop && (
+            <IconButton color="default">
+              <MenuIcon sx={{ fontSize: 32 }} />
+            </IconButton>
+          )}
+        </Container>
+      </Toolbar>
+    </AppBar>
   );
 }
 
