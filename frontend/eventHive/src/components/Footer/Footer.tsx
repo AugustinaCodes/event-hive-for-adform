02892dbb738ad2e073198d3be47695676c33603b
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-30 18:00:39 +0300

    working on footer

--- frontend/eventHive/src/components/Footer/Footer.tsx
+++ frontend/eventHive/src/components/Footer/Footer.tsx
@@ -2,19 +2,34 @@ import { Box, Container, Typography, Link } from "@mui/material";
 import EventHiveLogo from "../../assets/EventHiveLogo.svg";
 import styles from "./Footer.module.scss";
 
-
 export default function Footer() {
   return (
-    <Box component="footer" className={styles.footer}>
-        <Container>
-            <img src={EventHiveLogo} alt="Event Hive Logo" className={styles.logo} />
-            <Typography variant="body2" color="textSecondary" sx={{ mx: 1}}>
-                <Link href="/" color="inherit" sx={{ mx: 1}}>Home</Link>
-                <Link href="/" color="inherit" sx={{ mx: 1}}>Sponsors</Link>
-                <Link href="/" color="inherit" sx={{ mx: 1}}>Contacts</Link>
-                <Link href="/" color="inherit" sx={{ mx: 1}}>Rules</Link>
-            </Typography>
-        </Container>
+    <Box component="footer" className={styles.footer} >
+      <Container disableGutters sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+        <img
+          src={EventHiveLogo}
+          alt="Event Hive Logo"
+          className={styles.logo}
+        />
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          sx={{ mx: 1, display: "flex", flexDirection: "column", rowGap: "20px", justifyContent: "center", alignItems: "center", margin: "10px" }}
+        >
+          <Link href="/" >
+            Home
+          </Link>
+          <Link href="/" >
+            Sponsors
+          </Link>
+          <Link href="/">
+            Contacts
+          </Link>
+          <Link href="/" >
+            Rules
+          </Link>
+        </Typography>
+      </Container>
     </Box>
-  )
+  );
 }
