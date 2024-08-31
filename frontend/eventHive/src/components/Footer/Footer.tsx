7e8316b833d28b011fbd3cd818f4879c8a4eb413
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-31 21:28:53 +0300

    implemented hambruger menu modal

--- frontend/eventHive/src/components/Footer/Footer.tsx
+++ frontend/eventHive/src/components/Footer/Footer.tsx
@@ -4,8 +4,16 @@ import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
-    <Box component="footer" className={styles.footer} >
-      <Container disableGutters sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+    <Box component="footer" className={styles.footer}>
+      <Container
+        disableGutters
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <img
           src={EventHiveLogo}
           alt="Event Hive Logo"
@@ -14,18 +22,26 @@ export default function Footer() {
         <Typography
           variant="body2"
           color="textSecondary"
-          sx={{ mx: 1, display: "flex", flexDirection: "column", rowGap: "20px", justifyContent: "center", alignItems: "center", margin: "10px" }}
+          sx={{
+            mx: 1,
+            display: "flex",
+            flexDirection: "column",
+            rowGap: "20px",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "10px",
+          }}
         >
-          <Link href="/" >
+          <Link href="/" underline="hover" color="white">
             Home
           </Link>
-          <Link href="/" >
+          <Link href="/" underline="hover" color="white">
             Sponsors
           </Link>
-          <Link href="/">
+          <Link href="/" underline="hover" color="white">
             Contacts
           </Link>
-          <Link href="/" >
+          <Link href="/" underline="hover" color="white">
             Rules
           </Link>
         </Typography>
--- frontend/eventHive/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/eventHive/src/components/NavigationBar/NavigationBar.tsx
@@ -1,20 +1,33 @@
 import {
   AppBar,
+  Box,
   Button,
   Container,
   IconButton,
+  Modal,
   Toolbar,
   useMediaQuery,
 } from "@mui/material";
 import EventHiveTentLogo from "../../assets/EventHiveTentLogo.svg";
 import EventHiveLogo from "../../assets/EventHiveLogo.svg";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import styles from "./NavigationBar.module.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function NavigationBar() {
   const navigate = useNavigate();
   const isDesktop = useMediaQuery(`(min-width:900px)`);
+  const [openModal, setOpenModal] = useState(false);
+
+  function handleOpenModal() {
+    setOpenModal(true);
+  }
+
+  function handleCloseModal() {
+    setOpenModal(false);
+  }
 
   return (
     <AppBar
@@ -69,12 +82,57 @@ export default function NavigationBar() {
             Sign Up
           </Button>
           {!isDesktop && (
-            <IconButton color="default">
-              <MenuIcon sx={{ fontSize: 32 }} />
+            <IconButton
+              color="default"
+              onClick={openModal ? handleCloseModal : handleOpenModal}
+            >
+              {openModal ? (
+                <CloseIcon sx={{ fontSize: 32 }} />
+              ) : (
+                <MenuIcon sx={{ fontSize: 32 }} />
+              )}
             </IconButton>
           )}
         </Container>
       </Toolbar>
+
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "115px",
+            left: "83%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            flexDirection: "column",
+            width: "150px",
+            bgcolor: "background.paper",
+            borderRadius: "3px",
+          }}
+        >
+          <Button
+            color="inherit"
+            sx={{ fontWeight: "bold", display: "block" }}
+            onClick={handleCloseModal}
+          >
+            About
+          </Button>
+          <Button
+            color="inherit"
+            sx={{ fontWeight: "bold", display: "block" }}
+            onClick={handleCloseModal}
+          >
+            Events
+          </Button>
+          <Button
+            color="inherit"
+            sx={{ fontWeight: "bold", display: "block" }}
+            onClick={handleCloseModal}
+          >
+            Contact
+          </Button>
+        </Box>
+      </Modal>
     </AppBar>
   );
 }
